5bd949cbfc21f41650484e0b3949d639763b29f1
Shilpi <svvar31@gmail.com>
2024-12-20 11:59:34 +0530

    created service registry & registered micro services

--- UserService/src/main/java/com/business/user_service/UserServiceApplication.java
+++ UserService/src/main/java/com/business/user_service/UserServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableEurekaClient: It is no longer required; Gets automatically enabled;
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
